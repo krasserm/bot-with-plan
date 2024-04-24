5a7bc6c17ea5bd665048605c1c851aaa4322a5de
Martin Krasser <krasserm@googlemail.com>
2024-04-24 06:35:44 +0200

    Fix Llama-3 chat template

--- gba/client/chat.py
+++ gba/client/chat.py
@@ -7,6 +7,19 @@ from gba.client.base import Client, Message
 from gba.utils import prop_order_from_schema
 
 
+class Llama3Instruct(ChatWrapper):
+    @property
+    def _llm_type(self) -> str:
+        return "llama-3-instruct"
+
+    sys_beg: str = "<|start_header_id|>system<|end_header_id|>\n\n"
+    sys_end: str = "<|eot_id|>"
+    ai_n_beg: str = "<|start_header_id|>assistant<|end_header_id|>\n\n"
+    ai_n_end: str = "<|eot_id|>"
+    usr_n_beg: str = "<|start_header_id|>user<|end_header_id|>\n\n"
+    usr_n_end: str = "<|eot_id|>"
+
+
 class MistralInstruct(ChatWrapper):
     @property
     def _llm_type(self) -> str:
@@ -59,11 +72,18 @@ class ChatClient(Client):
         return messages_impl
 
     def complete(self, messages: List[Message], schema=None, **kwargs) -> Message:
+        if schema is None:
+            schema_args = {}
+        else:
+            schema_args = {
+                "schema": schema,
+                "prop_order": prop_order_from_schema(schema),
+            }
+
         response = self.model.invoke(
             input=self.convert_messages(messages),
-            schema=schema,
-            prop_order=prop_order_from_schema(schema),
             prompt_ext=self.model.ai_n_beg,
+            **schema_args,
             **kwargs,
         )
 
